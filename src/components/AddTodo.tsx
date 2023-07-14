19bb5f4eeafc5e26ef2626fb30552c97a5bb2af0
choiruladamm <choiruladamm@gmail.com>
2023-07-14 16:58:19 +0700

    Create Component Input and using forwardRef

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
+import { Input } from './Input'
 
 export const AddTodo = () => {
   const [input, setInput] = useState<string>('')
@@ -18,17 +19,16 @@ export const AddTodo = () => {
   return (
     <form onSubmit={handleSubmission}>
       <div className="flex items-center w-full max-w-lg gap-2 p-5 m-auto">
-        <input
+        <Input
           ref={inputRef}
           value={input}
           onChange={e => setInput(e.target.value)}
           type="text"
           placeholder="start typing your todo list..."
-          className="w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-white"
         />
         <button
           type="submit"
-          className="bg-white text-slate-900 px-5 py-2 text-sm font-normal rounded-xl active:scale-95 border-2 border-white"
+          className="bg-white text-slate-900 px-5 py-2 text-sm font-normal rounded-full active:scale-95 border-2 border-white"
         >
           Submit
         </button>

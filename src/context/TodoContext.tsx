68c43460c75f550cff09131ce5092d63b9e0199b
choiruladamm <choiruladamm@gmail.com>
2023-07-15 19:49:10 +0700

    adds a new todo using useState and a custom function

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,16 +1,28 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 interface TodoContextProps {
   todos: string[]
   addTodo: (text: string) => void
 }
 
-export const TodoContext = createContext<undefined>(undefined)
+export const TodoContext = createContext<TodoContextProps | undefined>(
+  undefined,
+)
 
 export const TodoProvider = (props: { children: React.ReactNode }) => {
+  const [todos, setTodos] = useState<string[]>([])
+
+  // add new todo method
+  const addTodo = (text: string) => {
+    setTodos([...todos, text])
+  }
+
+  const value: TodoContextProps = {
+    todos,
+    addTodo,
+  }
+
   return (
-    <TodoContext.Provider value={undefined}>
-      {props.children}
-    </TodoContext.Provider>
+    <TodoContext.Provider value={value}>{props.children}</TodoContext.Provider>
   )
 }

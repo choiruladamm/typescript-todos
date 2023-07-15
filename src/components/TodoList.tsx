c8eaf5dd5fed9a0bf744a90b640189d9b9e3a479
choiruladamm <choiruladamm@gmail.com>
2023-07-15 20:13:12 +0700

    Display Todo Item

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,25 @@
+import { useTodo } from '../context/useTodo'
+import { SiStarship } from 'react-icons/si'
+
 export const TodoList = () => {
-  return <div>TodoList</div>
+  const { todos } = useTodo()
+
+  if (!todos.length) {
+    return (
+      <div className="max-w-lg px-5 m-auto">
+        <h1 className="flex flex-col items-center gap-5 px-5 py-10 text-xl font-bold text-center rounded-xl bg-zinc-900">
+          <SiStarship className="text-5xl" />
+          you have nothing todo!
+        </h1>
+      </div>
+    )
+  }
+
+  return (
+    <ul className="grid max-w-lg gap-2 px-5 m-auto">
+      {todos.map(todo => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </ul>
+  )
 }

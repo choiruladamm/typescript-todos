5db9c8054dbc607d02464ac122f21e164c601583
choiruladamm <choiruladamm@gmail.com>
2023-07-15 19:36:01 +0700

    Refactor Code & Add interface Todo Context

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,5 +1,10 @@
 import React, { createContext } from 'react'
 
+interface TodoContextProps {
+  todos: string[]
+  addTodo: (text: string) => void
+}
+
 export const TodoContext = createContext<undefined>(undefined)
 
 export const TodoProvider = (props: { children: React.ReactNode }) => {

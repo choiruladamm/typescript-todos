fd134c7eda7ff94fcffb904391abde93012f7966
choiruladamm <choiruladamm@gmail.com>
2023-07-19 23:20:34 +0700

    add new todo method

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -19,17 +19,6 @@ export const TodoContext = createContext<TodoContextProps | undefined>(
 export const TodoProvider = (props: { children: React.ReactNode }) => {
   const [todos, setTodos] = useState<Todo[]>([])
 
-  // add new todo method
-  const addTodo = (text: string) => {
-    const newTodo: Todo = {
-      id: nanoid(),
-      text,
-      status: 'undone',
-    }
-
-    setTodos([...todos, newTodo])
-  }
-
   const value: TodoContextProps = {
     todos,
     addTodo,

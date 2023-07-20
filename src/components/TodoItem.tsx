aa8ad6849dbd9fd4117248de09925787e9c93219
choiruladamm <choiruladamm@gmail.com>
2023-07-20 22:17:14 +0700

    Styling Button Delete & Functionality

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,9 +1,22 @@
 import { motion } from 'framer-motion'
 import { Todo } from '../context/TodoContext'
 import cn from 'classnames'
+import { useTodo } from '../context'
+import { useRef } from 'react'
+import { toast } from 'react-hot-toast'
+import { RiDeleteBin7Line } from 'react-icons/ri'
 
 export const TodoItem = (props: { todo: Todo }) => {
   const { todo } = props
+
+  const { deleteTodo, editTodo, updateTodoStatus } = useTodo()
+
+  const editInputRef = useRef<HTMLInputElement>(null)
+
+  const handleDelete = (todoId: string) => {
+    deleteTodo(todoId)
+    toast.success('todo deleted succesfully!')
+  }
 
   return (
     <motion.li
@@ -21,6 +34,21 @@ export const TodoItem = (props: { todo: Todo }) => {
       >
         {todo.text}
       </motion.span>
+      <div className="flex justify-between gap-5 text-white">
+        {/* button update */}
+
+        {/* button status */}
+
+        {/* button delete */}
+        <button
+          onClick={() => handleDelete(todo.id)}
+          className="flex items-center gap-2 text-red-500
+        "
+        >
+          <RiDeleteBin7Line />
+          Delete
+        </button>
+      </div>
     </motion.li>
   )
 }

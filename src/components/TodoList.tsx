8f686ceb5c039d6fb74741d19907eaf71fe2ea7e
choiruladamm <choiruladamm@gmail.com>
2023-07-26 08:25:28 +0700

    Update Icons

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { TodoItem } from './TodoItem'
 import { useTodo } from "../context";
-import { SiStarship } from 'react-icons/si'
+import { IoCloudyNight } from "react-icons/io5";
 
 export const TodoList = () => {
   const { todos } = useTodo()
@@ -10,7 +10,7 @@ export const TodoList = () => {
     return (
       <div className="max-w-lg px-5 m-auto">
         <h1 className="flex flex-col items-center gap-5 px-5 py-10 text-xl font-bold text-center rounded-xl bg-zinc-900">
-          <SiStarship className="text-5xl" />
+          <IoCloudyNight className="text-5xl" />
           you have nothing todo!
         </h1>
       </div>
